55e92174d23729b60658dfaa099ca8eff4c00cff
Alex <tugalovakobir0@gmail.com>
2026-09-23 22:06:16 +0900

    feat: integration of enveromentall veriables

--- task.js
+++
@@ -1,8 +0,0 @@
-//MITTASK-L
-function string(type) {
-  return type
-    .split(" ")
-    .map((wort) => wort.split("").reverse().join(""))
-    .join(" ");
-}
-console.log(string("salom !dunyo hammaga Assalomu alaykum"));
