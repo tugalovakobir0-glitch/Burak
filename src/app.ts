ef9dff0bc2451a5d0da5acd8b0b732c99a1c2d27
Alex <tugalovakobir0@gmail.com>
2026-09-25 17:07:22 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 /** 1- ENTRANCE- kirish qismi **/
 const app = express();
@@ -14,5 +15,6 @@ app.use(express.json());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 /** 1- ROUTERS- ruterlar qismi **/
+app.use("/", router);
 
 export default app;
---
+++ src/controls/member.conteroller.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import { T } from "../libs/types/common";
+
+const memberController: T = {};
+
+memberController.goHome = (req: Request, res: Response) => {
+  try {
+    res.send("Home Page");
+  } catch (err) {
+    console.log("Error Home Page:", err);
+  }
+};
+memberController.getLogin = (req: Request, res: Response) => {
+  try {
+    res.send("Login Page");
+  } catch (err) {
+    console.log("Error Login Page:", err);
+  }
+};
+memberController.getSignup = (req: Request, res: Response) => {
+  try {
+    res.send("Signup Page");
+  } catch (err) {
+    console.log("Error Signup Page:", err);
+  }
+};
+
+export default memberController;
