292134e7fe0e87e49b2619dd6354546678346e41
Alex <tugalovakobir0@gmail.com>
2026-09-25 22:32:06 +0900

    feat: Completed schema.model

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import mongoose from "mongoose";
-mongoose.set("strictQuery", true);
+// mongoose.set("strictQuery", true);
 import app from "./app";
 
 mongoose
